2fbf16b4f60a75016a560102e2a0bdb8ff31358b
Rajdwip-69 <143189655+Rajdwip-69@users.noreply.github.com>
2026-03-15 23:23:03 +0530

    Add files via upload

---
+++ Day1_Code.py
@@ -0,0 +1,14 @@
+# name = "Rajdwip" #String
+# age = 23 #int 
+# height = 5.5 # float 
+# is_student = True  # Boolean (True / false )
+
+# print(name)
+# print(age)
+# print(height)
+# print(is_student)
+
+#Addition of 2 Number ----- > 
+"""a = 5 
+b = 6 
+print(a+b)"""
