eaafee3316bc6634aeaaf54b81e730622005d8f1
Rajdwip-69 <143189655+Rajdwip-69@users.noreply.github.com>
2026-03-18 23:12:09 +0530

    Add files via upload

---
+++ Day2_Code.py
@@ -0,0 +1,84 @@
+# Arithmatic Oprator ------>
+
+# a = 10
+# b = 3 
+
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)
+# print(a**b)
+
+
+# Comparison Operator ---- >
+
+# a = 5 
+# b = 10
+
+# print(a == b)
+# print(a != b)
+# print(a>b)
+# print(a<b)
+# print(a>=b)
+# print(a<=b)
+
+
+
+#Logical Oprator ----  >
+
+# x = True 
+# y = False 
+
+# print (x and y)
+# print ( x or y)
+# print (not y)
+
+
+#Assignment Operator ----->
+
+# a = 3
+
+# # a += 5 
+
+# print(a)
+
+# a *= 5
+# print(a)
+
+
+# Bitwise Oprator ----- >
+
+# a = 5
+# b = 3
+# print(a&b)
+# print(a|b)
+# print(a<<1)
+# print(a>>1)
+
+
+#Membership operator --------->
+
+# list1 = [1,2,3,4]
+# print(2 in list1)
+# print(5 not in list1)
+# print(6 in list1)
+
+
+#Identity Operatror ------------>
+
+# a = [1,2]
+# b = a 
+# c = [1,3]
+
+# print(a is b)
+# print(a is c)
+# print(a is not b)
+# print(a is not  c)
+
+
+
+
+
+
+
