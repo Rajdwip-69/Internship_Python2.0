4f13085e9dd9cf3f2f90e1276771d278b8f3cb12
Rajdwip-69 <143189655+Rajdwip-69@users.noreply.github.com>
2026-03-22 19:31:41 +0530

    Add files via upload

---
+++ Day3_Code.py
@@ -0,0 +1,124 @@
+# String in Python ----- > 
+
+
+# a = "Rajdwip"
+# print(a[0])
+# print(a[6])
+
+# #Find Length
+
+# print(len(a))
+
+# txt = "Hello , I am Learning Python"
+# print("i" in txt)
+
+
+# b = "Hello, World"
+# # print(b[2:8])
+# # print(b[2:])
+
+# print(b[11]) # 0 Based Indexing 
+# print(b[-12]) # 1 Based Indexing 
+
+# a = "Rajdwip"
+# print(a[::-1])
+
+# a = "hello"
+# print(a.upper())
+
+# a = "HELLO"
+# print(a.lower())
+
+# a = " Hello, World "
+# print(a.strip())
+
+
+# a = "Hello World"
+# print(a.replace("H","J"))
+
+# a = "hello"
+
+# b = "World"
+
+# c = a+b
+
+# print(c)
+
+
+# String Formatting
+
+# age = 35
+# txt = "My name is Jhon, I am " + str(age)
+# print(txt)
+
+# F string 
+
+# age = 36
+# txt = f"My name is John , i am {age} "
+# print(txt)
+
+
+# price = 59
+# txt = f"The price is {price} dollars"
+# print(txt)
+
+# age = 35
+# print("My age is", age)
+
+# a = 2+3j
+# print(type(a))
+
+
+# List in Python 
+
+
+# thislist = ["apple","banana" , "cherry","apple"]
+# print(thislist)
+# print(type(thislist))
+# print(thislist[0:0])
+
+# thislist = ["apple",1,3.4,1+2j,True]
+# print(thislist)
+
+# thislist = ["apple","banana" ,"cherry"]
+# thislist[1] = "kiwi"
+# print(thislist)
+
+# thislist = ["apple","banana" , "cherry"]
+# thislist.insert(2,"kiwi")
+# print(thislist)
+
+# thislist = ["apple","banana" , "cherry",]
+# thislist.append("orange")
+# print(thislist)
+
+
+# thislist = ["apple","banana" , "cherry",]
+# thistuple = ("kiwi","orange")
+# thislist.extend(thistuple)
+# print(thislist)
+
+# thislist = ["apple","banana" , "cherry",]
+# thislist.remove("banana")
+# print(thislist)
+
+# thislist = ["apple","banana" , "cherry",]
+# thislist.pop()
+# print(thislist)
+
+# thislist = ["apple","banana" , "cherry"]
+# print(thislist.clear())
+
+# thislist = ["orange" , "kiwi" ,"apple","banana" , "cherry","Avogardo","Elephant"]
+# thislist.sort()
+# print(thislist)
+
+# thislist = ["apple","banana" , "cherry"]
+# mylist = thislist.copy()
+# print(mylist)
+
+
+
+
+
+
